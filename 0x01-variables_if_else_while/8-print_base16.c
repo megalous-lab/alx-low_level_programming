b2e1db4e613163b05f1367e766fb2345d9e4b733
megalous-lab <yekeenabdmuizz@gmail.com>
2022-07-01 14:30:04 -0700

    Base 16 letters in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - This program prints all numbers in base 16 in lc
+ *and uses the function putchar
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char p;
+	int i;
+
+	for (p = '0'; p <= '9'; p++)
+		putchar(p);
+	for (i = 97; i < == 102; i++)
+		putchar((int)i);
+
+	putchar('\n');
+	return (0);
+}
