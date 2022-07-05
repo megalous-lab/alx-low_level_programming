893e713dd027d19c0cede6e051dedb5ddfd58ee9
megalous-lab <yekeenabdmuizz@gmail.com>
2022-07-05 08:43:44 -0700

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  * main - Prints alphabet in lowercase
  * Return: always 0
  */
-int main(void)
+void print_alphabet(void)
 {
 	char al;
 	for (al = 'a'; al <= 'z'; al++)
-		_putchar(p);
+		_putchar(al);
 	
 	_putchar('\n');
 	return (0);
