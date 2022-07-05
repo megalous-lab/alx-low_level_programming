9797de69b6722271c6debee2accb6359803a0d69
megalous-lab <yekeenabdmuizz@gmail.com>
2022-07-05 08:41:49 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Prints alphabet in lowercase
+ * Return: always 0
+ */
+int main(void)
+{
+	char al;
+	for (al = 'a'; al <= 'z'; al++)
+		_putchar(p);
+	
+	_putchar('\n');
+	return (0);
+}
