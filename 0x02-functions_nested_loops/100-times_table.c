d9905ff931479680321a6b49bca620b72ddc88f5
megalous-lab <yekeenabdmuizz@gmail.com>
2022-07-05 14:50:54 -0700

    Time tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,34 +1,48 @@
 #include "main.h"
 
 /**
- * times_table - Prints time table
+ * print_times_table - Print the `n` times table, starting with 0
+ * If `n` is greater than 15 or less than 0, print nothing
+ * @n: int type number
  */
-void print_times_table(int)
+void print_times_table(int n)
 {
-	int x, y, k;
+	int x = 0, y, z;
 
-	for (x = 0; x < 10; x++)
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		for (y = 0; y < 10; y++)
+		for (y = 0; y <= n; y++)
 		{
-			k = x * y;
-			if (y == 0)
-				_putchar(k + '0');
-			if (y != 0 && k < 10)
+			z = x * y;
+			if (z > 99)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
 			}
-			else if (k >= 10)
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+			if (y != n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
+		x++;
 	}
 }
