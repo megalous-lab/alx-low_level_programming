a55e284f62a002db4f212edc0ace592d29494092
megalous-lab <yekeenabdmuizz@gmail.com>
2022-07-05 12:21:44 -0700

    Hot fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  * @c: character to check
  * Return: 1 if c is an alphabet, otherwise Return: 0
  */
-int _isalpha(int c)
+int _isalpha(int c),
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
