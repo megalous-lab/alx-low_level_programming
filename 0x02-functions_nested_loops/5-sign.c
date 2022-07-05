0407f98d66cfc73ca94bece39115e96f995b707d
megalous-lab <yekeenabdmuizz@gmail.com>
2022-07-05 13:13:41 -0700

    Hot fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,18 +13,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		result (1);
+		return (1);
 	}
 
 	else if (n == 0)
 	{
 		_putchar('0');
-		result (0);
+		return (0);
 	}
 
 	else
 	{
 		_putchar('-');
-		result (-1);
+		return (-1);
 	}
 }
