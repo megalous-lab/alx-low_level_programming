c56d18dc9dd7925f8cbca04f08cdb7c576e394d5
megalous-lab <yekeenabdmuizz@gmail.com>
2022-07-14 10:19:39 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,14 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char leet[] = "oOlL22eEaA5566tT";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j < 16; j++)
+			if (s[i] == leet[j])
+				s[i] = j / 2 + '0';
 	}
 
 	return (s);
