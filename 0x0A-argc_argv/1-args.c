10192da0f3bc0407a77673ca4667dde7adff9165
megalous-lab <yekeenabdmuizz@gmail.com>
2022-07-22 15:40:02 -0700

    Print no of arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: no of command line arg
+ * @argv: Array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	
+	return (0);
+}
