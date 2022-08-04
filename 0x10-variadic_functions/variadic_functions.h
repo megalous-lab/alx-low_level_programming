57c2dc264d3f8bdd9e679a70fd3797d5fa99e1f3
megalous-lab <yekeenabdmuizz@gmail.com>
2022-08-04 14:59:20 -0700

    variadic header added

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
+int sum_them_all(const unsigned int n, ...);
+
+
+#endif
